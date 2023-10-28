c6fe7f36ad6bc3e79bdc394d54efe16110325ed4
Kirbyrawr <devkirbyrawr@gmail.com>
2023-10-28 22:49:11 +0200

    Fixes and improvements
    - Added zoom in and out to the preview.
    - Added buttons to reset original size and fit in image.
    - Removed scaling of the image to prevent messing up in pixel art.
    - Added panning to the preview.
    - Improved performance by adding a check by md5 instead of a string of the full image. (Thanks Jare for the tip).

--- pykrita/rgb565preview/rgb565preview.py
+++ pykrita/rgb565preview/rgb565preview.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import Qt, QThread, pyqtSignal, QTimer
 from PyQt5.QtGui import QImage, QPixmap, QImageReader
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QHBoxLayout, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem, QLabel
+import hashlib
 from krita import Krita, DockWidget, DockWidgetFactory, DockWidgetFactoryBase
 
 KI = Krita.instance()
@@ -21,9 +22,36 @@ class ImageProcessingThread(QThread):
         buffer[:] = ptr.asarray()
         return QImage(buffer, width, height, QImage.Format_RGB16)
 
-def get_scaled_image(image, scale_factor):
-    scaled_image = image.scaled(int(image.width() * scale_factor), int(image.height() * scale_factor))
-    return scaled_image
+def get_image_hash(image):
+    data = image.bits().asstring(image.byteCount())
+    return hashlib.md5(data).hexdigest()
+
+class CustomGraphicsView(QGraphicsView):
+    zoomChanged = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def wheelEvent(self, event):
+        # Capture the current scale factor before applying zoom
+        current_scale = self.transform().m11()
+
+        zoom_factor = 1.1  # Adjusted to slow down the zoom speed
+        if event.angleDelta().y() > 0:  # Zoom in
+            self.scale(zoom_factor, zoom_factor)
+        else:  # Zoom out
+            if current_scale > 0.25:  # Ensure we're not zooming out below 25%
+                self.scale(1 / zoom_factor, 1 / zoom_factor)
+        self.zoomChanged.emit()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.setDragMode(QGraphicsView.ScrollHandDrag)
+        super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        self.setDragMode(QGraphicsView.NoDrag)
+        super().mouseReleaseEvent(event)
 
 class RGB565Preview(DockWidget):
     def __init__(self):
@@ -31,9 +59,31 @@ class RGB565Preview(DockWidget):
         self.setWindowTitle("RGB565 Preview")
 
         layout = QVBoxLayout()
-        self.previewLabel = QLabel(self)
-        self.previewLabel.setAlignment(Qt.AlignCenter)
-        layout.addWidget(self.previewLabel)
+
+        self.view = CustomGraphicsView(self)
+        self.scene = QGraphicsScene(self)
+        self.view.setScene(self.scene)
+        self.pixmapItem = QGraphicsPixmapItem()
+        self.scene.addItem(self.pixmapItem)
+
+        layout.addWidget(self.view)
+
+        # Create a horizontal layout for control buttons and label
+        controlLayout = QHBoxLayout()
+
+        restoreBtn = QPushButton("Original Size")
+        restoreBtn.clicked.connect(self.restoreOriginalSize)
+        controlLayout.addWidget(restoreBtn)
+
+        fitBtn = QPushButton("Fit Image")
+        fitBtn.clicked.connect(self.fitImageInView)
+        controlLayout.addWidget(fitBtn)
+
+        self.zoomLabel = QLabel("100%")
+        controlLayout.addWidget(self.zoomLabel)
+
+        layout.addLayout(controlLayout)
+
         widget = QWidget(self)
         widget.setLayout(layout)
         self.setWidget(widget)
@@ -46,6 +96,25 @@ class RGB565Preview(DockWidget):
         self.timer.timeout.connect(self.checkForUpdate)
         self.timer.start(1000)  # Update every 1 second to reduce frequency
 
+        # For panning
+        self.view.setDragMode(QGraphicsView.NoDrag)  # Disable the default drag mode
+
+        # Connect the zoomChanged signal to update the zoom label
+        self.view.zoomChanged.connect(self.updateZoomLabel)
+
+    def restoreOriginalSize(self):
+        self.view.resetTransform()
+        self.updateZoomLabel()
+
+    def fitImageInView(self):
+        self.view.fitInView(self.pixmapItem, Qt.KeepAspectRatio)
+        self.updateZoomLabel()
+
+    def updateZoomLabel(self):
+        # Calculate the zoom percentage
+        zoom_percentage = round(self.view.transform().m11() * 100)
+        self.zoomLabel.setText(f"{zoom_percentage}%")
+
     def canvasChanged(self, canvas):
         pass
 
@@ -53,14 +122,13 @@ class RGB565Preview(DockWidget):
         doc = KI.activeDocument()
         if doc:
             image = doc.projection(0, 0, doc.width(), doc.height()).convertToFormat(QImage.Format_RGBA8888)
-            scaled_image = get_scaled_image(image, 0.5)  # Scale down by 50%
-            currImageData = scaled_image.bits().asstring(scaled_image.byteCount())
-            if self.prevImageData != currImageData:
-                self.processingThread.run(scaled_image)
-                self.prevImageData = currImageData
+            currImageHash = get_image_hash(image)
+            if self.prevImageData != currImageHash:
+                self.processingThread.run(image)
+                self.prevImageData = currImageHash
 
     def updatePreview(self, resultImage):
         pixmap = QPixmap.fromImage(resultImage)
-        self.previewLabel.setPixmap(pixmap.scaled(self.previewLabel.width(), self.previewLabel.height(), Qt.KeepAspectRatio))
+        self.pixmapItem.setPixmap(pixmap)
 
 KI.addDockWidgetFactory(DockWidgetFactory("rgb565preview", DockWidgetFactoryBase.DockRight, RGB565Preview))
